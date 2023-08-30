80d8b08fbdc253a3a44d75b5b24285d6f3280ebc
sherna90 <sherna90@yahoo.es>
2023-08-30 17:28:14 -0400

    py script

---
+++ beijing_multi_site.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import glob 
+import numpy as np 
+import jax 
+import jax.numpy as jnp 
+import optax
+from flax import linen as nn
+from functools import partial
+from sklearn import metrics
+
+def get_dataloader(X,y,batch_size,key,axis=0):
+    num_train=X.shape[axis]
+    indices = jnp.array(list(range(0,num_train)))
+    indices=jax.random.permutation(key,indices)
+    for i in range(0, len(indices),batch_size):
+        batch_indices = jnp.array(indices[i: i+batch_size])
+        yield X[:,batch_indices,:], y[:,batch_indices]
+
+def pre_process(df):
+    df['date_time']=pd.to_datetime(df[['year', 'month', 'day','hour']])
+    df.drop(columns=['year', 'month', 'day','hour','No'],inplace=True)
+    df.set_index('date_time',inplace=True)
+    return df
+
+def train_test_split(data,split_fraction,feature_keys):
+    data=data[feature_keys]
+    train_split = int(split_fraction * int(data.shape[0]))
+    data_mean = data[:train_split].mean(axis=0)
+    data_std = data[:train_split].std(axis=0)
+    data = (data - data_mean) / data_std
+    train_data = data.iloc[0 : train_split - 1]
+    val_data = data.iloc[train_split:]
+    return train_data,val_data
+
+def create_batch(data,lag,future):
+    df_lag=pd.concat([data[:-future].shift(i) for i in range(lag-1,-1,-1)],axis=1).dropna()
+    #df_lag.columns=['pm_'+str(i) for i in range(lag,-1,1)]
+    X=df_lag.values
+    y=data[future+lag-1:].values
+    return X,y
+
+def create_batch_multistep(data,lag,future):
+    df_lag=pd.concat([data[:-future].shift(i) for i in range(lag-1,-1,-1)],axis=1).dropna()
+    df_future=pd.concat([data[lag-1:].shift(-i) for i in range(1,future+1)],axis=1).dropna()
+    #df_lag.columns=['pm_'+str(i) for i in range(lag,-1,1)]
+    X=df_lag.values
+    y=df_future.values
+    return X,y
+
+def log_likelihood(params, x, y):
+    preds = jax.vmap(model.apply, (0, 0))(params, jnp.array(x[:,:,:,np.newaxis]))
+    return jnp.mean(optax.l2_loss(y,preds).sum(axis=-1))
+
+@partial(jax.jit, static_argnums=(3,4))
+def sgld_kernel_momemtum(key, params, momemtum,grad_log_post, dt, X, y_data):
+    gamma,eps=0.9,1e-6
+    key, subkey = jax.random.split(key, 2)
+    grads = grad_log_post(params, X, y_data)
+    squared_grads=jax.tree_map(lambda g: jnp.square(g),grads)
+    momemtum=jax.tree_map(lambda m,s : gamma*m+(1-gamma)*s,momemtum,squared_grads)
+    noise=jax.tree_map(lambda p: jax.random.normal(key=subkey,shape=p.shape), params)
+    params=jax.tree_map(lambda p, g,m,n: p-0.5*dt*g/(m+eps)+jnp.sqrt(dt)*n, params, grads,momemtum,noise)
+    return key, params,momemtum
+
+def sgld(key,log_post, grad_log_post, num_samples,
+                             dt, x_0,X_train_datasets,y_train_datasets,batch_size,
+                             test_data=None):
+    samples = list()
+    loss=list()
+    param = x_0
+    key_data, key_model = jax.random.split(key, 2)
+    momemtum=jax.tree_map(lambda p:jnp.zeros_like(p),param)
+    key_data_batch=jax.random.split(key_data, num_samples)
+    for i in range(num_samples):
+        train_data=get_dataloader(X_train_datasets,y_train_datasets,batch_size,key_data_batch[i],axis=1)
+        for _,(X_batch, y_batch) in enumerate(train_data):
+            key_model,param,momemtum = sgld_kernel_momemtum(key_model, param,momemtum, grad_log_post, dt, X_batch, y_batch)
+        loss.append(log_post(param,X_batch,y_batch))
+        samples.append(param)
+        if (i%(num_samples//10)==0):
+            print('iteration {0}, loss {1:.2f}'.format(i,loss[-1]))
+    return samples,loss
+
+# read data
+path = 'data/PRSA_Data_20130301-20170228/'
+csv_files = glob.glob(path + "/*.csv")
+df_list = [pd.read_csv(file) for file in csv_files]
+df_preprocessed=[pre_process(df) for df in df_list]
+df_group=pd.concat(df_preprocessed)
+
+# create train test datasets
+split_fraction = 0.8
+feature_keys = ['PM2.5']
+X_train_datasets=list()
+y_train_datasets=list()
+X_test_datasets=list()
+y_test_datasets=list()
+future=6
+past=6
+dataset_names=list()
+for item_id, gdf in df_group.groupby('station'):
+    gdf.fillna(method='ffill',inplace=True)
+    train,test=train_test_split(gdf,split_fraction,['PM2.5'])
+    X_train,y_train=create_batch_multistep(train,past,future)
+    X_test,y_test=create_batch_multistep(test,past,future)
+    X_train_datasets.append(X_train)
+    y_train_datasets.append(y_train)
+    X_test_datasets.append(X_test)
+    y_test_datasets.append(y_test)
+    dataset_names.append(item_id)
+X_train_datasets=np.stack(X_train_datasets,axis=0)
+y_train_datasets=np.stack(y_train_datasets,axis=0)
+X_test_datasets=np.stack(X_test_datasets,axis=0)
+y_test_datasets=np.stack(y_test_datasets,axis=0)
+
+class LSTM(nn.Module):
+    features: int
+    output: int
+
+    @nn.remat    
+    @nn.compact   
+    def __call__(self, X_batch):
+        carry,x=nn.RNN(nn.LSTMCell(self.features),return_carry=True)(X_batch)
+        #carry,x=nn.RNN(nn.LSTMCell(self.features),return_carry=True)(x)
+        x=nn.Dense(self.output)(x)
+        return x[:,-1,:]
+
+key=jax.random.PRNGKey(0)
+key_model,key_data=jax.random.split(key,2)
+batch_size=256
+model=LSTM(32,future)
+n_groups=X_train_datasets.shape[0]
+inputs = jax.random.randint(key,(batch_size,past,1),0, 10,).astype(jnp.float32)
+key_tasks=jax.random.split(key_model,n_groups)
+params_tasks = jax.vmap(model.init, (0, None))(key_tasks, inputs)
+dt=1e-5
+grad_log_post=jax.jit(jax.grad(log_likelihood))
+samples,loss=sgld(key_data,log_likelihood, grad_log_post, 50,
+                             dt, params_tasks,X_train_datasets,y_train_datasets,
+                             batch_size,test_data=None)
+
+X_test=X_test_datasets[:,:,:,np.newaxis]
+params=samples[35]
+preds=jax.vmap(model.apply, (0, 0))(params, X_test)
+r_metric=list()
+rmse_metric=list()
+mae_metric=list()
+for i in range(n_groups):
+    r_squared=metrics.r2_score(preds[i,:,:],y_test_datasets[i,:,:])
+    rmse=metrics.mean_squared_error(preds[i,:,:],y_test_datasets[i,:,:],squared=True)
+    mae=metrics.mean_absolute_error(preds[i,:,:],y_test_datasets[i,:,:])
+    r_metric.append(r_squared)
+    rmse_metric.append(rmse)
+    mae_metric.append(mae)
+    print('task : {0}, RMSE : {1:1.2f}, MAE :{2:1.2f}, R2 :{3:1.2f}'.format(dataset_names[i],rmse,mae,r_squared))
+print('-------------------------------------------------------')
+print('RMSE : {0:1.2f}, MAE :{1:1.2f}, R2 :{2:1.2f}'.format(np.mean(rmse_metric),np.mean(mae_metric),np.mean(r_metric)))
