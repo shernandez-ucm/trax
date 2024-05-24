6df33a9b46500e0f8fd99dddd23152b73c31aee7
sherna90 <sherna90@yahoo.es>
2024-05-24 15:09:18 -0400

    commit

--- normal_model.py
+++ normal_model.py
@@ -1,4 +1,4 @@
-import arviz as az
+#import arviz as az
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -31,29 +31,29 @@ def log_likelihood(params, data):
     params['weights']=jax.nn.softmax(params['weights'])
     params['sigmas']=jax.vmap(jax.nn.softplus)(params['sigmas'])
     ll_per_data = jax.vmap(partial(loglike_across_components,params['weights'],params['mus'],params['sigmas']))(data)
-    return -1.0*jnp.sum(ll_per_data)
+    return -1.0*jnp.mean(ll_per_data)
 
 def log_prior(params):
-    log_prior=jax.tree.map(lambda p: distrax.Normal(0.0,1.0).log_prob(p), params['mus']).sum()
+    log_prior=jax.tree_util.tree_map(lambda p: distrax.Normal(0.0,1.0).log_prob(p), params['mus']).sum()
     log_prior+=distrax.Dirichlet(jnp.ones(3)).log_prob(params['weights'])
     return log_prior
 
 def log_post(params,batch):
     n_data=batch.shape[0]
-    return -1./n_data*log_prior(params) + log_likelihood(params,batch)
+    return -n_data*log_prior(params) + log_likelihood(params,batch)
 
 grad_log_post=jax.jit(jax.grad(log_post))
 
-@partial(jax.jit, static_argnums=(3,4))
+#@partial(jax.jit, static_argnums=(3,4))
 def sgld_kernel_momemtum(key, params, momemtum,grad_log_post, dt,batch):
     gamma,eps=0.9,1e-6
     key, subkey = jax.random.split(key, 2)
     grads = grad_log_post(params, batch)
-    norm_grads=jax.tree.map(lambda g: g/jnp.linalg.norm(g),grads)
-    squared_grads=jax.tree.map(lambda g: jnp.square(g),grads)
-    momemtum=jax.tree.map(lambda m,s : gamma*m+(1-gamma)*s,momemtum,squared_grads)
-    noise=jax.tree.map(lambda p: jax.random.normal(key=subkey,shape=p.shape), params)
-    params=jax.tree.map(lambda p, g,m,n: p-0.5*dt*g/(m+eps)+jnp.sqrt(dt)*n, params, grads,momemtum,noise)
+    norm_grads=jax.tree_util.tree_map(lambda g: g/jnp.linalg.norm(g),grads)
+    squared_grads=jax.tree_util.tree_map(lambda g: jnp.square(g),grads)
+    momemtum=jax.tree_util.tree_map(lambda m,s : gamma*m+(1-gamma)*s,momemtum,squared_grads)
+    noise=jax.tree_util.tree_map(lambda p: jax.random.normal(key=subkey,shape=p.shape), params)
+    params=jax.tree_util.tree_map(lambda p, g,m,n: p-0.5*dt*g/(m+eps)+jnp.sqrt(dt)*n, params, grads,momemtum,noise)
     return key, params,momemtum
 
 def sgld(key,log_post, grad_log_post, num_samples,
@@ -63,7 +63,7 @@ def sgld(key,log_post, grad_log_post, num_samples,
     param = x_0
     key_data, key_model = jax.random.split(key, 2)
     schedule_fn = lambda k: dt * k ** (-0.55)
-    momemtum=jax.tree_map(lambda p:jnp.zeros_like(p),param)
+    momemtum=jax.tree_util.tree_map(lambda p:jnp.zeros_like(p),param)
     key_data_batch=jax.random.split(key_data, num_samples)
     for i in range(1,num_samples+1):
         train_data=get_dataloader(X,batch_size,key_data_batch[i])
@@ -93,7 +93,7 @@ key_mu,key_sigma=jax.random.split(key_model,2)
 params={'weights':jnp.ones(3)/3.,
         'mus':jax.random.normal(key_mu,shape=(3,)),
         'sigmas':jax.random.normal(key_sigma,shape=(3,))}
-samples,loss=sgld(key_data,log_post, grad_log_post, 5_000,1e-3,params,X,32)
+samples,loss=sgld(key_data,log_post, grad_log_post, 5_000,1e-3,params,X,16)
 
 posterior_means=jnp.stack([s['mus'] for s in samples])
 posterior_weights=jnp.stack([s['weights'] for s in samples])
